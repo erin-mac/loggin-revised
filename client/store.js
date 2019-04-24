9ffd91df7542fdefb05a6a5e7212b683f9d544fc
Erin McAweeney <ermcaweeney@gmail.com>
2019-04-23 20:00:09 -0400

    halfway (?) done

--- client/store.js
+++ client/store.js
@@ -1,10 +1,45 @@
-import {createStore, applyMiddleware, combineReducers} from 'redux'
+import { createStore, applyMiddleware, combineReducers } from 'redux'
 import loggerMiddleware from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 
+//Action creator
 
-const userReducer = (state = { }, action) => {
-  return state;
+const SET_USER = 'SET_USER'
+
+const setUser = users => ({
+  type: SET_USER,
+  users
+})
+
+
+const login = ({ email, password }) => dispatch => {
+  try {
+    const response = axios.post('/auth/', { email, password })
+    return dispatch(setUser(response))
+  } catch (error) { console.log(error) }
+}
+
+const getUser = () => dispatch => {
+  try {
+    const response = axios.get('/auth/')
+    return dispatch(setUser(response))
+  } catch (error) { console.log(error) }
+}
+
+const logout = () => dispatch => {
+  try {
+    const response = axios.delete('/auth/')
+    return dispatch(setUser({}))
+  } catch (error) { console.log(error) }
+}
+
+const userReducer = (state = { email: 'emcaweeney@gmail.com', password: '123' }, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return (user)
+    default:
+      return state
+  }
 }
 
 const reducer = combineReducers({
